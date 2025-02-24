039544469bc41f0ad53b2f457fe798297cbc6e86
goeunP <0young384@gmail.com>
2025-02-24 15:18:21 +0900

    feat(#7): 돼지저금통 추천주식 애니메이션 추가

--- src/pages/PiggybankPage/PiggybankPage.jsx
+++ src/pages/PiggybankPage/PiggybankPage.jsx
@@ -1,3 +1,11 @@
+import Piggybank from "./components/Piggybank";
+import CurrentMileage from "./components/CurrentMileage";
+
 export default function PiggybankPage() {
-  return <div>PiggybankPage</div>;
+  return (
+    <div className="flex flex-col items-center gap-20 overflow-hidden">
+      <CurrentMileage />
+      <Piggybank />
+    </div>
+  );
 }
