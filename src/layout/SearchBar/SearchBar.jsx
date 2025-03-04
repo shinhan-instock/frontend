b33cb949982216a88e26bfb8f499a629e0ab863d
goeunP <0young384@gmail.com>
2025-03-04 18:24:10 +0900

    feat/#33-서치바 모달 분리

--- src/layout/SearchBar/SearchBar.jsx
+++ src/layout/SearchBar/SearchBar.jsx
@@ -1,47 +1,16 @@
-import { useState, useRef } from "react";
-import searchIcon from "/img/searchIcon.png";
-import Modal from "../../components/common/Modal";
-import {
-  UserSearchResultList,
-  StockSearchResultList,
-} from "./components/SearchResultList";
-import TopStock from "./components/TopStock";
+/* eslint-disable react/prop-types */
+import { CiSearch } from "react-icons/ci";
 
-export default function SearchBar() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
-  // 0: 검색 전, 1: 주식 검색, 2: 인물 검색
-  const [searchType, setSearchType] = useState(0);
-
-  const inputRef = useRef(null); // useRef로 input을 참조
-
-  const handleSearch = (e) => {
-    if (e.key == "Enter") {
-      if (searchInput.charAt(0) == "₩") {
-        setSearchType(1);
-      } else {
-        setSearchType(2);
-      }
-    }
-  };
-
-  const handleFocus = () => {
-    setSearchInput(""); // 포커스 시 입력값 초기화
-  };
-
+export default function SearchBar({ setIsSearchOpen }) {
   const handleButtonClick = () => {
-    setIsModalOpen(true);
-    if (inputRef.current) {
-      inputRef.current.focus(); // 버튼 클릭 시 input에 포커스 주기
-    }
+    setIsSearchOpen(true);
   };
 
   return (
     <>
       <div className="flex flex-col items-center p-4">
         <div className="w-4/5 border-1 border-stroke-gray py-1 px-5 rounded-md flex flex-row items-center gap-3">
-          <img src={searchIcon} className="w-[20px] h-[20px]" />
-
+          <CiSearch />
           <button
             onClick={handleButtonClick}
             className="w-full focus:outline-none text-left text-stroke-gray"
@@ -50,33 +19,6 @@ export default function SearchBar() {
           </button>
         </div>
       </div>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <div className="flex flex-col items-center">
-          <div className="w-11/12 border-1 border-stroke-gray bg-instock-gray py-1 px-5 rounded-md flex flex-row items-center gap-3">
-            <img src={searchIcon} className="w-[20px] h-[20px]" />
-            <input
-              ref={inputRef} // useRef로 input 참조
-              placeholder="주식을 검색하려면 검색어에 ₩를 붙여주세요"
-              onChange={(e) => {
-                setSearchInput(e.target.value);
-              }}
-              onKeyDown={(e) => {
-                handleSearch(e);
-              }}
-              onFocus={handleFocus} // 입력 필드가 포커스되면 입력값 초기화
-              className="w-full focus:outline-none"
-              value={searchInput}
-            ></input>
-          </div>
-          {searchType === 0 ? (
-            <TopStock />
-          ) : searchType === 1 ? (
-            <StockSearchResultList />
-          ) : (
-            <UserSearchResultList />
-          )}
-        </div>
-      </Modal>
     </>
   );
 }
--- src/pages/MyProfileEditPage/MyProfileEditPage.jsx
+++ src/pages/MyProfileEditPage/MyProfileEditPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import EditForm from "./components/EditForm";
 import Modal from "../../components/common/Modal";
+import crypig from "/img/crypig.png";
 
 export default function MyProfileEditPage() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +20,13 @@ export default function MyProfileEditPage() {
         </button>
       </div>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        asdfasdfasdf
+        <div className="flex flex-col items-center justify-center h-4/5 gap-10">
+          <img src={crypig} className="w-1/3 h-1/3 " />
+          <div className="text-2xl">정말 계정을 삭제 하시겠습니까?</div>
+          <button className="bg-black text-white rounded-4xl py-2 px-16 ">
+            네
+          </button>
+        </div>
       </Modal>
     </>
   );
