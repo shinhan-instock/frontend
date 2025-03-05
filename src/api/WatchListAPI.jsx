03b7730fdce9d8930221185d62f4107844fa3027
goeunP <0young384@gmail.com>
2025-03-05 11:15:17 +0900

    feat/#39-login context 추가

---
+++ src/api/WatchListAPI.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:8080";
+
+// export async function getWatchList(page, size, userId) {
+//   const res = await axios.get(`${BASE_URL}/watchList`);
+//   const data = res.data.result;
+//   return data;
+// }
---
+++ src/context/loginContext.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+
+export const loginContext = createContext();
+
+function instockUser() {
+  const instock_user = localStorage.getItem("instock_user");
+  return instock_user !== "" ? JSON.parse(instock_user) : null;
+}
+export function LoginProvider({ children }) {
+  const [userInfo, setUserInfo] = useState(instockUser());
+
+  return (
+    <>
+      <loginContext.Provider value={{ userInfo, setUserInfo }}>
+        {children}
+      </loginContext.Provider>
+    </>
+  );
+}
