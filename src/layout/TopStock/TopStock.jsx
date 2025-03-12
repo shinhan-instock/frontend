295f91a2ce8e46a6dc5970bc7a79111af9f3e3b7
campus3 S036 <s036@alpacoedu.onmicrosoft.com>
2025-03-12 17:43:27 +0900

    feat/#65-검색어순위top10api연결및디자인수정'

--- src/layout/TopStock/TopStock.jsx
+++ src/layout/TopStock/TopStock.jsx
@@ -35,12 +35,35 @@ export default function TopStock() {
 
   useEffect(() => {
     getTop10Stocks().then((data) => {
-      // console.log("jiwon:", data);
       const _stockData = [];
 
       if (sessionStorage.getItem("top_10_rank")) {
-        //s
+        const prevRankings = JSON.parse(sessionStorage.getItem("top_10_rank"));
         
+        data.forEach((stock, data_idx) => {
+          let top = '';
+
+          prevRankings.forEach((item, prev_idx) => {
+            if (stock.stockCode === item.id) {
+              if (data_idx < prev_idx) {
+                top = '▲';
+              } else if (data_idx > prev_idx) {
+                top = '▼';
+              } else {
+                top = '-';
+              }
+            }
+          });
+
+          _stockData.push({
+            id: stock.stockCode,
+            name: stock.stockName,
+            img: `https://static.toss.im/png-icons/securities/icn-sec-fill-${stock.stockCode}.png`,
+            top: (top === '' ? '▲' : top)
+          });
+        });
+        setStockData(_stockData);
+        sessionStorage.setItem("top_10_rank", JSON.stringify(_stockData));
       } else {
         data.forEach((stock) => {
           _stockData.push({
@@ -51,7 +74,7 @@ export default function TopStock() {
           });
         });
         setStockData(_stockData);
-        sessionStorage.setItem("top_10_rank", _stockData);
+        sessionStorage.setItem("top_10_rank", JSON.stringify(_stockData));
       }
     });
   }, []);
@@ -59,23 +82,25 @@ export default function TopStock() {
   return (
     <div className="w-full mx-auto px-5">
       <div className="flex flex-col p-4 rounded-lg bg-instock-gray">
-        <div className="flex mb-4 justify-center font-bold">실시간 검색어</div>
+        <div className="flex mb-4 justify-center font-title">실시간 검색어</div>
 
-        <div className="flex flex-col h-64 overflow-auto scrollbar-hide">
+        <div className="flex flex-col h-64 overflow-auto
+          ">
           {stockData.map((stock) => (
             <div
               key={stock.id}
-              className="flex justify-between space-y-3 w-full pb-2 pr-2"
-            >
+              className="flex justify-between space-y-3 w-full cursor-pointer hover:bg-gray-200 p-2 rounded-md"
+              onClick={() => window.location.href = `/stock/${encodeURIComponent(stock.stockName)}`}
+              >
               <div className="flex flex-row space-x-3 w-full">
                 <img
                   src={stock.img}
                   alt={stock.name}
-                  className="w-13 h-13 rounded-full"
+                  className="w-10 h-10 rounded-full"
                 />
                 <div className="flex justify-between w-full">
                   <div className="flex w-full items-center">
-                    <p className="text-m font-bold">{stock.name}</p>
+                    <p className="text-sm font-bold">{stock.name}</p>
                   </div>
                   <div className="flex justify-center items-center">
                     {stock.top}
