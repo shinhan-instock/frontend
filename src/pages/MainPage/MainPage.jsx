499c6c0c9d1c3a1fedb187aa173b128120d66013
goeunP <0young384@gmail.com>
2025-03-13 23:26:13 +0900

    feat/#93-로그인한 유저만 post create

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
-import PostList from '../../components/common/PostList';
-import NavigationBar from '../../components/common/NavigationBar';
-import PostCreate from '../MainPage/components/PostCreate';
-import MainPostsData from './components/MainPostsData';
+import { useState } from "react";
+import PostList from "../../components/common/PostList";
+import NavigationBar from "../../components/common/NavigationBar";
+import PostCreate from "../MainPage/components/PostCreate";
+import MainPostsData from "./components/MainPostsData";
+import { useLogin } from "../../hooks/useLogin";
 
 export default function MainPage() {
   const [selectedTab, setSelectedTab] = useState(1);
   const [postsData, setPostsData] = useState([]);
+  const { userInfo } = useLogin();
   return (
     <div className="flex flex-col items-center h-screen">
       <div className="sticky w-full flex flex-row justify-center">
-        <PostCreate />
+        {userInfo && <PostCreate />}
       </div>
       <div className="w-4/5">
         {/* <div className="sticky flex w-full  bg-white border-b border-zinc-300 z-10"> */}
